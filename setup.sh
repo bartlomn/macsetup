3803b99c20086bb34c8a02177c13039139b241c6
GitHub <>
2023-01-16 15:05:23 +0100

    Dock and finder setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 
 # 
+# DOCK CONFIG
+#
+
+# Autohide and magnification for dock
+osascript -e "tell application \"System Events\" to set the autohide of the dock preferences to true"
+osascript -e "tell application \"System Events\" to set the magnification of the dock preferences to true"
+
+# When you quit an app, the icon will disappear from your Dock
+defaults write com.apple.dock static-only -bool true
+
+# Show hidden apps
+defaults write com.apple.dock showhidden -bool true
+
+# 
 # FINDER CONFIG
 #
 
-# Autohide finder
-osascript -e "tell application \"System Events\" to set the autohide of the dock preferences to true"
+# Show Library Folder in Finder
+chflags nohidden ~/Library
+
+# Show Hidden Files in Finder
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show all extensions in Finder
+defaults write com.apple.finder AppleShowAllExtensions -bool true
+
+# Show Path Bar in Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show Status Bar in Finder
+defaults write com.apple.finder ShowStatusBar -bool true
