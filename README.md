2af0ccd76c83877e5011fcb6dfa54eb39bd30f0a
Bart <bnowak@bnowak.com>
2024-09-16 15:43:25 +0200

    start ubuntu config script

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Mac setup utility script
 
-## Running
+## Running on Mac
 
 ```bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/bartlomn/macsetup/main/setup.sh)"
+```
+
+## Running on Ubuntu
+
+```bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/bartlomn/macsetup/main/ubuntu_setup.sh)"
 ```
---
+++ ubuntu_setup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# 
+# SUDO
+# 
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+done 2>/dev/null &
+
+
+# 
+# APT
+#
+sudo apt update
+sudo apt upgrade -y
+
+# 
+# OH MY ZSH
+# 
+# if [ -d ~/.oh-my-zsh ]; then
+# 	echo "oh-my-zsh is already installed"
+#  else
+#  	echo "installing oh-my-zsh"
+#     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#     # then Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc.
+#     mv ~/.zshrc ~/.zshrc.bak
+#     sed 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc.bak > ~/.zshrc
+#     # and enable  plugins: plugins=(aws docker docker-compose encode64 git helm kubectl zsh-autosuggestions zsh-syntax-highlighting)
+#     mv ~/.zshrc ~/.zshrc.bak
+#     sed 's/^plugins=.*$/plugins=(aws docker docker-compose encode64 git helm kubectl zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc.bak > ~/.zshrc
+#     # Download required fonts
+#     cd /Library/Fonts || exit
+#     curl -L -O "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf" \
+#     -L -O "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf" \
+#     -L -O "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf" \
+#     -L -O "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
+#     cd ~ || exit
+# fi
+
